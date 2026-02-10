20bb3f84dfbc356038b3221f1b9dd8f969fdab16
SamChavez <sa5m.escom@gmail.com>
2026-02-09 20:07:11 -0600

    build: envs on runtime

--- app/infrastructure/database/session.py
+++ app/infrastructure/database/session.py
@@ -1,15 +1,23 @@
 from sqlalchemy.orm import sessionmaker, Session
-from app.infrastructure.database.engine import engine
+from app.infrastructure.database.engine import get_engine
 
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine,
-)
+
+_session = None
+
+
+def get_session():
+    global _SessionLocal
+    if _SessionLocal is None:
+        _SessionLocal = sessionmaker(
+            autocommit=False,
+            autoflush=False,
+            bind=get_engine(),
+        )
+    return _SessionLocal
 
 
 def get_db() -> Session:
-    db = SessionLocal()
+    db = get_session()()
     try:
         yield db
     finally:
